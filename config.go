af9fd6175bbe586fa3aa5f431917fa4ae2f68fe8
M4cs <mabridgland@protonmail.com>
2020-05-22 17:04:20 -0400

    Add Color Configuration

--- config.go
+++ config.go
@@ -1,6 +1,8 @@
 package main
 
 type Config struct {
+	ShowUser bool `json:"showUser"`
+	ShowSep bool `json:"showSep"`
 	ShowMem bool `json:"showMem"`
 	ShowTotalCPUCores bool `json:"showTotalCPUCores"`
 	ShowTotalCPUThreads bool `json:"showTotalCPUThreads"`
@@ -10,10 +12,30 @@ type Config struct {
 	ShowBios bool `json:"showBios"`
 	ShowBaseboard bool `json:"showBaseboard"`
 	ShowAscii bool `json:"showAscii"`
+	UseDefaultColors bool `json:"useDefaultColors"`
+	AsciiColor string `json:"asciiValue"`
+	UserColor string `json:"userColor"`
+	SepColor string `json:"sepColor"`
+	TitleColor string `json:"titleColor"`
+	InfoColor string `json:"infoColor"`
+	Titles TitleValues `json:"titles"`
+}
+
+type TitleValues struct {
+	Memory string `json:"memory"`
+	CPUCores string `json:"cpuCores"`
+	CPUThreads string `json:"cpuThreads"`
+	GPUs string `json:"gpus"`
+	DiskSize string `json:"diskSize"`
+	WindowsVersion string `json:"windowsVersion"`		
+	Bios string `json:"bios"`
+	Baseboard string `json:"baseboard"`
 }
 
 func newConfig() Config {
 	config := Config{}
+	config.ShowUser = true
+	config.ShowSep = true
 	config.ShowMem = true
 	config.ShowTotalCPUCores = true
 	config.ShowTotalCPUThreads = true
@@ -23,5 +45,19 @@ func newConfig() Config {
 	config.ShowBios = true
 	config.ShowBaseboard = true
 	config.ShowAscii = true
+	config.UseDefaultColors = true
+	config.AsciiColor = "Blue"
+	config.UserColor = "Red"
+	config.SepColor = "Red"
+	config.TitleColor = "Green"
+	config.InfoColor = "White"
+	config.Titles.Memory = "Memory"
+	config.Titles.CPUCores = "CPU Cores"
+	config.Titles.CPUThreads = "CPU Threads"
+	config.Titles.GPUs = "GPU #"
+	config.Titles.DiskSize = "Disk Size"
+	config.Titles.WindowsVersion = "Windows Ver."
+	config.Titles.Bios = "BIOS"
+	config.Titles.Baseboard = "Baseboard"
 	return config
 }
--- main.go
+++ main.go
@@ -13,17 +13,71 @@ import (
 
 	"github.com/jaypipes/ghw"
 	"golang.org/x/sys/windows/registry"
-	"github.com/fatih/color"
+	"github.com/gilliek/go-xterm256/xterm256"
 )
+
+func getCustomColor(color string) xterm256.Color {
+	var (
+		Black       = xterm256.Color{ForegroundColor: 0, BackgroundColor: -1}
+		DarkRed     = xterm256.Color{ForegroundColor: 1, BackgroundColor: -1}
+		DarkGreen   = xterm256.Color{ForegroundColor: 2, BackgroundColor: -1}
+		DarkYellow  = xterm256.Color{ForegroundColor: 3, BackgroundColor: -1}
+		DarkBlue    = xterm256.Color{ForegroundColor: 4, BackgroundColor: -1}
+		DarkMagenta = xterm256.Color{ForegroundColor: 5, BackgroundColor: -1}
+		DarkCyan    = xterm256.Color{ForegroundColor: 6, BackgroundColor: -1}
+		LightGray   = xterm256.Color{ForegroundColor: 7, BackgroundColor: -1}
+		DarkGray    = xterm256.Color{ForegroundColor: 8, BackgroundColor: -1}
+		Red         = xterm256.Color{ForegroundColor: 9, BackgroundColor: -1}
+		Green       = xterm256.Color{ForegroundColor: 10, BackgroundColor: -1}
+		Yellow      = xterm256.Color{ForegroundColor: 11, BackgroundColor: -1}
+		Blue        = xterm256.Color{ForegroundColor: 12, BackgroundColor: -1}
+		Magenta     = xterm256.Color{ForegroundColor: 13, BackgroundColor: -1}
+		Cyan        = xterm256.Color{ForegroundColor: 14, BackgroundColor: -1}
+		White       = xterm256.Color{ForegroundColor: 15, BackgroundColor: -1}
+	
+		Orange = xterm256.Color{ForegroundColor: 130, BackgroundColor: -1}
+	)
+	switch color {
+	case "Black":
+		return Black
+	case "DarkRed":
+		return DarkRed
+	case "DarkGreen":
+		return DarkGreen
+	case "DarkYellow":
+		return DarkYellow
+	case "DarkBlue":
+		return DarkBlue
+	case "DarkMagenta":
+		return DarkMagenta
+	case "DarkCyan":
+		return DarkCyan
+	case "LightGray":
+		return LightGray
+	case "DarkGray":
+		return DarkGray
+	case "Red":
+		return Red
+	case "Green":
+		return Green
+	case "Yellow":
+		return Yellow
+	case "Blue":
+		return Blue
+	case "Magenta":
+		return Magenta
+	case "Cyan":
+		return Cyan
+	case "White":
+		return White
+	case "Orange":
+		return Orange
+	}
+	return White
+}
 
 func main() {
 	var s []string
-	green := color.New(color.FgGreen).SprintFunc()
-	red := color.New(color.FgRed, color.Bold).SprintFunc()
-	blue := color.New(color.FgBlue, color.Bold).SprintFunc()
-	yellow := color.New(color.FgYellow).SprintFunc()
-	magenta := color.New(color.FgMagenta).SprintFunc()
-	cyan := color.New(color.FgCyan).SprintFunc()
 	var winArt string = `                                ..,
                     ....,,:;+ccllll
       ...,,+:;  cllllllllllllllllll
@@ -64,14 +118,30 @@ llllllllllllll  lllllllllllllllllll
 	if err = jsonParser.Decode(&config); err != nil{
 		log.Fatal("Error Parsing Config File: ", err.Error())
 	}
-	s = append(s, red(user.Username))
-	s = append(s, red("----------------"))
+
+	title := xterm256.Green
+	ascii := xterm256.Blue
+	sep := xterm256.Red
+	userc := xterm256.Red
+	info := xterm256.White
+
+	if (!config.UseDefaultColors) {
+		title = getCustomColor(config.TitleColor)
+		ascii = getCustomColor(config.AsciiColor)
+		sep = getCustomColor(config.SepColor)
+		userc = getCustomColor(config.UserColor)
+		info = getCustomColor(config.InfoColor)
+	}
+
+	s = append(s, xterm256.Sprint(userc, strings.ReplaceAll(user.Username, "\\", "@")))
+	s = append(s, xterm256.Sprint(sep, "--------------------------------"))
+
 	memorySplit := strings.Split(memory.String(), "(")
 	mem := strings.Split(memorySplit[1], ",")
 	usableMem := strings.Split(mem[1], "usable")
 	physMem := strings.Split(mem[0], "physical")
 	if (config.ShowMem) {
-		s = append(s, green("Memory: ") + strings.ReplaceAll(usableMem[0], "MB ", "GB") + "/" + strings.ReplaceAll(physMem[0], "MB", "GB"))
+		s = append(s, xterm256.Sprint(title, config.Titles.Memory + ": ") + xterm256.Sprint(info, strings.ReplaceAll(usableMem[0], "MB ", "GB") + "/" + strings.ReplaceAll(physMem[0], "MB", "GB")))
 	}
 	if (config.ShowTotalCPUCores || config.ShowTotalCPUThreads){
 		cpu, err := ghw.CPU()
@@ -79,10 +149,10 @@ llllllllllllll  lllllllllllllllllll
 			fmt.Printf("Error getting CPU info: %v", err)
 		}
 		if (config.ShowTotalCPUCores){
-			s = append(s, green("Total CPU Cores: ") +  fmt.Sprint(cpu.TotalCores))
+			s = append(s, xterm256.Sprint(title, config.Titles.CPUCores + ": ") +   xterm256.Sprint(info, fmt.Sprint(cpu.TotalCores)))
 		}
 		if (config.ShowTotalCPUThreads){
-			s = append(s, green("Total CPU Threads: ") + fmt.Sprint(cpu.TotalThreads))
+			s = append(s, xterm256.Sprint(title, config.Titles.CPUThreads + ": ") +  xterm256.Sprint(info, fmt.Sprint(cpu.TotalThreads)))
 		}
 	}
 	if (config.ShowWindowsVersion){
@@ -92,14 +162,14 @@ llllllllllllll  lllllllllllllllllll
 		if err != nil {
 			log.Fatal(err)
 		}
-		s = append(s, green("Windows Version: ") + pn)
+		s = append(s, xterm256.Sprint(title, config.Titles.WindowsVersion + ": ") + xterm256.Sprint(info, pn))
 	}
 	if (config.ShowTotalDiskSize){
 		bi, err := ghw.Block()
 		if err != nil {
 			fmt.Printf("Error getting disk info: %v", err)
 		}
-		s = append(s, green("Total Disk Size: ") + ByteFormat(float64(bi.TotalPhysicalBytes), 1))
+		s = append(s, xterm256.Sprint(title, config.Titles.DiskSize + ": ") +  xterm256.Sprint(info, ByteFormat(float64(bi.TotalPhysicalBytes), 1)))
 	}
 	if (config.ShowGPUS){
 		gpu, err := ghw.GPU()
@@ -108,7 +178,7 @@ llllllllllllll  lllllllllllllllllll
 		}
 		gpuin := 0
 		for _, c := range gpu.GraphicsCards {
-			s = append(s, green("GPU #" + fmt.Sprint(gpuin)) +": " + c.DeviceInfo.Product.Name)
+			s = append(s, xterm256.Sprint(title, config.Titles.GPUs + ": " + fmt.Sprint(gpuin) + ": ") +  xterm256.Sprint(info, c.DeviceInfo.Product.Name))
 			gpuin++
 		}
 	}
@@ -120,25 +190,26 @@ llllllllllllll  lllllllllllllllllll
 		if err != nil {
 			fmt.Printf("Error getting BIOS info: %v", err)
 		}
-		s = append(s, green("BIOS: ") + bios.Vendor)
+		s = append(s, xterm256.Sprint(title, config.Titles.Bios + ": ") +  xterm256.Sprint(info, bios.Vendor))
 	}
 	if (config.ShowBaseboard){
 		bb, err := ghw.Baseboard()
 		if err != nil {
 			fmt.Printf("Error getting BB info: %v", err)
 		}
-		s = append(s, green("Baseboard: ")  + bb.Vendor)
+		s = append(s, xterm256.Sprint(title, config.Titles.Baseboard + ": ")  + xterm256.Sprint(info, bb.Vendor))
 	}
 	s = append(s, "")
-	s = append(s, "     " + red("███") + green("███") + yellow("███") + blue("███") + magenta("███") + cyan("███"))
+	s = append(s, "    " + xterm256.Sprint(xterm256.LightGray, "███") + xterm256.Sprint(xterm256.Red, "███") + xterm256.Sprint(xterm256.Green, "███") + xterm256.Sprint(xterm256.Yellow, "███") + xterm256.Sprint(xterm256.Blue, "███") + xterm256.Sprint(xterm256.Magenta, "███") + xterm256.Sprint(xterm256.Cyan, "███"))
+	s = append(s, "    " + xterm256.Sprint(xterm256.DarkGray, "███") + xterm256.Sprint(xterm256.DarkRed, "███") + xterm256.Sprint(xterm256.DarkGreen, "███") + xterm256.Sprint(xterm256.DarkYellow, "███") + xterm256.Sprint(xterm256.DarkBlue, "███") + xterm256.Sprint(xterm256.DarkMagenta, "███") + xterm256.Sprint(xterm256.DarkCyan, "███"))
 	scanner := bufio.NewScanner(strings.NewReader(winArt))
 	index := 0
 	for i, str := range s {
-		fmt.Println(blue(winArtResult[i]) + "    " + str)
+		fmt.Println(xterm256.Sprint(ascii, winArtResult[i]) + "    " + str)
 	}
 	for scanner.Scan() {
 		if index >= len(s) {
-			fmt.Println(blue(scanner.Text()))
+			fmt.Println(xterm256.Sprint(ascii, scanner.Text()))
 		}
 		index++
 	}
